d12bca4cf22e612a24894f07afede0250bd1cd74
SANTIAGO GONZALEZ RUIZ <santiagogonzalez319552@correo.itm.edu.co>
2024-03-23 13:46:59 -0500

    Segunda prueba con ayuda del vídeo y la guía

---
+++ Prueba2/Prueba2/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba2.Data.Entities;
+
+namespace Prueba2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Country> Country { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
